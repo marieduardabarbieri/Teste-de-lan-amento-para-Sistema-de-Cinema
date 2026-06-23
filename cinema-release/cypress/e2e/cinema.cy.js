e57f49d1bc402da78a4e315ff0d65ef8b51c9757
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-23 13:06:15 -0400

    Criação de todos os testes
    1 Teste - não deve permitir comprar um assento já ocupado
    2 Teste  - deve cancelar uma compra
    3 Teste  - deve permitir comprar novamente após cancelamento

--- cinema-release/cypress/e2e/cinema.cy.js
+++ cinema-release/cypress/e2e/cinema.cy.js
@@ -1,10 +1,44 @@
-describe("Release Testing - Cinema", () => {
+describe("Release Testing - Sistema de Cinema", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
 
   it("deve comprar um assento disponível", () => {
     cy.get("#assento").select("A1");
+
+    cy.get("#comprar").click();
+
+    cy.get("#mensagem").should("contain", "Compra realizada");
+  });
+
+  it("não deve permitir comprar um assento já ocupado", () => {
+    cy.get("#assento").select("A2");
+
+    cy.get("#comprar").click();
+
+    cy.get("#mensagem").should("contain", "Compra realizada");
+
+    cy.get("#comprar").click();
+
+    cy.get("#mensagem").should("contain", "Assento indisponível");
+  });
+
+  it("deve cancelar uma compra", () => {
+    cy.get("#assento").select("A3");
+
+    cy.get("#comprar").click();
+
+    cy.get("#cancelar").click();
+
+    cy.get("#mensagem").should("contain", "Compra cancelada");
+  });
+
+  it("deve permitir comprar novamente após cancelamento", () => {
+    cy.get("#assento").select("A4");
+
+    cy.get("#comprar").click();
+
+    cy.get("#cancelar").click();
 
     cy.get("#comprar").click();
 
