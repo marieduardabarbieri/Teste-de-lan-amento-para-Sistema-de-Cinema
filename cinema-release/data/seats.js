e27664ecacb53df2f72b828aa57d77e402c456c4
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-23 10:29:46 -0400

    Criação da pasta data com os assentos cadastrados

---
+++ cinema-release/data/seats.js
@@ -0,0 +1,9 @@
+const seats = [
+  { id: "A1", sold: false },
+  { id: "A2", sold: false },
+  { id: "A3", sold: false },
+  { id: "A4", sold: false },
+  { id: "A5", sold: false },
+];
+
+module.exports = seats;
