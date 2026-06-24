306280fcd1da9defeba9944743af34b2bd922f1c
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-24 10:26:03 -0400

    Criando mais assentos para integrar o CSS

--- cinema-release/data/assentos.js
+++ cinema-release/data/assentos.js
@@ -4,6 +4,18 @@ const assentos = [
   { id: "A3", ocupado: false },
   { id: "A4", ocupado: false },
   { id: "A5", ocupado: false },
+
+  { id: "B1", ocupado: false },
+  { id: "B2", ocupado: false },
+  { id: "B3", ocupado: false },
+  { id: "B4", ocupado: false },
+  { id: "B5", ocupado: false },
+
+  { id: "C1", ocupado: false },
+  { id: "C2", ocupado: false },
+  { id: "C3", ocupado: false },
+  { id: "C4", ocupado: false },
+  { id: "C5", ocupado: false },
 ];
 
 module.exports = assentos;
